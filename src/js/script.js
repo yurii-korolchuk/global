32327a7ffb4f7dc1e3ad0f2c957e37d934ceab5a
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-06-06 21:53:03 +0300

    Slider completed

--- src/js/script.js
+++ src/js/script.js
@@ -4,25 +4,42 @@ const slider = tns({
     items: 3,
     slideBy: 1,
     nav: false,
-    controls: false
-
+    controls: false,
+    animateDelay: 300
 });
 
+
 document.querySelector('.reviews-prev').onclick = function () {
-    var info = slider.getInfo(),
-      indexCurrent = info.index + 1;
-      info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
-    slider.goTo('prev');
-    var info2 = slider.getInfo(),
-      indexCurrent2 = info2.index + 1;
-    info.slideItems[indexCurrent2].classList.toggle('slider__item_passive');
+  var info = slider.getInfo(),
+  indexPrev = info.indexCached + 1,
+  indexCurrent = info.index;
+
+  info.slideItems[indexPrev].classList.toggle('slider__item_passive');
+  info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
+  console.log(info.slideItems[indexPrev]);
+  console.log(info.slideItems[indexCurrent]);
+  slider.goTo('prev');
+  if(info.slideItems[indexCurrent].classList.contains('slider__item_passive')) {
+    info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
+  }
+  if(!info.slideItems[indexCurrent - 1].classList.contains('slider__item_passive')){
+    info.slideItems[indexCurrent - 1].classList.toggle('slider__item_passive');
+  }
 };
 document.querySelector('.reviews-next').onclick = function () {
-    var info = slider.getInfo(),
-      indexCurrent = info.index + 1;
-      info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
-    slider.goTo('next');
-    var info2 = slider.getInfo(),
-      indexCurrent2 = info2.index + 1;
-    info.slideItems[indexCurrent2].classList.toggle('slider__item_passive');
+  var info = slider.getInfo(),
+  indexPrev = info.indexCached + 1,
+  indexCurrent = info.index + 2;
+
+  info.slideItems[indexPrev].classList.toggle('slider__item_passive');
+  info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
+  console.log(info.slideItems[indexPrev]);
+  console.log(info.slideItems[indexCurrent]);
+  slider.goTo('next');
+  if(info.slideItems[indexCurrent].classList.contains('slider__item_passive')) {
+    info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
+  }
+  if(!info.slideItems[indexCurrent + 1].classList.contains('slider__item_passive')){
+    info.slideItems[indexCurrent + 1].classList.toggle('slider__item_passive');
+  }
 };
