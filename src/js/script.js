222c75f130c70bc52c7b0e88fc63d738b6e50450
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-06-19 12:42:42 +0300

    Changed slider to slick slider, added custom classes to slider buttons

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,16 @@
-
-const slider = tns({
-    container: '.slider',
-    items: 3,
-    slideBy: 1,
-    nav: false,
-    controls: false,
-    animateDelay: 300
+document.addEventListener('DOMContentLoaded', () => {
+	$('.slider').slick({
+		centerMode: true,
+  		slidesToShow: 3,
+		prevArrow: $('.reviews-prev'),
+		nextArrow: $('.reviews-next')
+	});
+	document.querySelectorAll('.reviews button').forEach(item => {
+		item.addEventListener('click', (e) => {
+			e.target.classList.add('active');
+			setTimeout(() => {
+				e.target.classList.remove('active');
+			}, 300);
+		});
+	});
 });
-
-
-document.querySelector('.reviews-prev').onclick = function () {
-  var info = slider.getInfo(),
-  indexPrev = info.indexCached + 1,
-  indexCurrent = info.index;
-
-  info.slideItems[indexPrev].classList.toggle('slider__item_passive');
-  info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
-  console.log(info.slideItems[indexPrev]);
-  console.log(info.slideItems[indexCurrent]);
-  slider.goTo('prev');
-  if(info.slideItems[indexCurrent].classList.contains('slider__item_passive')) {
-    info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
-  }
-  if(!info.slideItems[indexCurrent - 1].classList.contains('slider__item_passive')){
-    info.slideItems[indexCurrent - 1].classList.toggle('slider__item_passive');
-  }
-};
-document.querySelector('.reviews-next').onclick = function () {
-  var info = slider.getInfo(),
-  indexPrev = info.indexCached + 1,
-  indexCurrent = info.index + 2;
-
-  info.slideItems[indexPrev].classList.toggle('slider__item_passive');
-  info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
-  console.log(info.slideItems[indexPrev]);
-  console.log(info.slideItems[indexCurrent]);
-  slider.goTo('next');
-  if(info.slideItems[indexCurrent].classList.contains('slider__item_passive')) {
-    info.slideItems[indexCurrent].classList.toggle('slider__item_passive');
-  }
-  if(!info.slideItems[indexCurrent + 1].classList.contains('slider__item_passive')){
-    info.slideItems[indexCurrent + 1].classList.toggle('slider__item_passive');
-  }
-};
