c8b050ac7decc81c4400596540e85466dac25627
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-07-01 00:33:35 +0300

    Fixed hamburger animation

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -178,15 +178,20 @@ function form(formSelector, modalWindowSelector) {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 function hamburger() {
-    document.querySelector('.hamburger__wrapper').addEventListener('click', (e) => {
-		if(e.target.parentNode.classList.contains('hamburger__wrapper_close')) {
-			e.target.parentNode.classList.remove('hamburger__wrapper_close');
-			document.querySelector('.nav').classList.remove('nav_active');
-			document.body.style.overflow = 'auto';
-		} else {
-			e.target.parentNode.classList.add('hamburger__wrapper_close');
-			document.querySelector('.nav').classList.add('nav_active');
-			document.body.style.overflow = 'hidden';
+    document.querySelector('.hamburger').addEventListener('click', (e) => {
+		if(e.target && e.target.classList.contains('hamburger__wrapper')) {
+			if(e.target.classList.contains('hamburger__wrapper_open')) {
+				e.target.classList.remove('hamburger__wrapper_open');
+				e.target.classList.add('hamburger__wrapper_close');
+				document.querySelector('.nav').classList.add('nav_active');
+				document.body.style.overflow = 'hidden';
+	
+			} else if(e.target.classList.contains('hamburger__wrapper_close')) {
+				e.target.classList.remove('hamburger__wrapper_close');
+				e.target.classList.add('hamburger__wrapper_open');
+				document.querySelector('.nav').classList.remove('nav_active');
+				document.body.style.overflow = 'auto';
+			}
 		}
 	});
 }
