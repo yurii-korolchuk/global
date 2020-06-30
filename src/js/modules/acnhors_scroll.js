ad886cce8c69692c4ea400cbd9b4ce4569885458
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-07-01 00:55:55 +0300

    Fixed some minor bugs in nav UI

--- src/js/modules/acnhors_scroll.js
+++ src/js/modules/acnhors_scroll.js
@@ -2,12 +2,18 @@ function anchorsNav(navSelector) {
     document.querySelectorAll(navSelector).forEach(item => {
         item.addEventListener('click', function(e) {
             e.preventDefault();
-
             const blockID = item.getAttribute('href').substr(1);
             document.getElementById(blockID).scrollIntoView({
                 behavior: 'smooth',
                 block: 'center'
             });
+            try {
+                if(document.querySelector('.nav').classList.contains('nav_active')) {
+                    document.querySelector('.nav').classList.remove('nav_active');
+                    document.querySelector('.hamburger__wrapper').classList.remove('hamburger__wrapper_close');
+                    document.querySelector('.hamburger__wrapper').classList.add('hamburger__wrapper_open');
+                }
+            } catch(e) {}
         });
     });
 }
