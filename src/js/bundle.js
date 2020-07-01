a03108d7062c7d9f2186959ffdf54ceb7da43a8f
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-07-01 23:13:23 +0300

    Moved open(close)NavMenu code into separate functions

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -183,20 +183,29 @@ function form(formSelector, modalWindowSelector) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+function openNavMenu(navMenuSelector, navMenuActiveClassSelector) {
+	document.querySelector(navMenuSelector).classList.add(navMenuActiveClassSelector);
+	document.body.style.overflow = 'hidden';
+}
+
+function closeNavMenu(navMenuSelector, navMenuActiveClassSelector) {
+	document.querySelector(navMenuSelector).classList.remove(navMenuActiveClassSelector);
+	document.body.style.overflow = 'auto';
+}
+
 function hamburger() {
     document.querySelector('.hamburger').addEventListener('click', (e) => {
 		if(e.target && e.target.classList.contains('hamburger__wrapper')) {
 			if(e.target.classList.contains('hamburger__wrapper_open')) {
 				e.target.classList.remove('hamburger__wrapper_open');
 				e.target.classList.add('hamburger__wrapper_close');
-				document.querySelector('.nav').classList.add('nav_active');
-				document.body.style.overflow = 'hidden';
+				openNavMenu('.nav', 'nav_active');
+				
 	
 			} else if(e.target.classList.contains('hamburger__wrapper_close')) {
 				e.target.classList.remove('hamburger__wrapper_close');
 				e.target.classList.add('hamburger__wrapper_open');
-				document.querySelector('.nav').classList.remove('nav_active');
-				document.body.style.overflow = 'auto';
+				closeNavMenu('.nav', 'nav_active');
 			}
 		}
 	});
